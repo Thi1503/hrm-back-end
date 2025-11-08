05d648fec9234f685a3136fb0790da1612406690
Thi1503 <thile15032003@gmail.com>
2025-11-09 06:53:54 +0700

    refactor Error code

--- identity-service/src/main/java/com/devteria/identityservice/exception/ErrorCode.java
+++ identity-service/src/main/java/com/devteria/identityservice/exception/ErrorCode.java
@@ -7,15 +7,22 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
-    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(1004, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
-    USER_NOT_EXISTED(1005, "User not existed", HttpStatus.NOT_FOUND),
-    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
+
+    /// Đầu 1xx bắt lỗi Exception
+    USER_EXISTED(102, "User existed", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(103, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(104, "User not existed", HttpStatus.NOT_FOUND),
+
+    /// Đầu 2xx bắt lỗi auth
+    UNCATEGORIZED_EXCEPTION(299, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNAUTHENTICATED(201, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(202, "You do not have permission", HttpStatus.FORBIDDEN),
+
+    ///  Đầu 3xx bắt lỗi validate
+    INVALID_PASSWORD(301, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(302, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
+    INVALID_KEY(303, "Uncategorized error", HttpStatus.BAD_REQUEST),
+
     ;
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
